e9bfb36303b625e80aa2ed5a5f7fd037eb1ef3b6
Cássio Silva Oliveira <cassio.silva.oliveira@volkswagen.de>
2022-03-28 14:50:52 +0200

    adjust frame rate

--- packages/camera/camera/ios/Classes/FLTCam.m
+++ packages/camera/camera/ios/Classes/FLTCam.m
@@ -173,6 +173,14 @@ NSString *const errorMethod = @"error";
   [self setCaptureSessionPreset:_resolutionPreset];
   [self updateOrientation];
 
+  NSError* configurationError = nil;
+  if([_captureDevice lockForConfiguration:&configurationError]) {
+    _captureDevice.activeVideoMinFrameDuration = CMTimeMake(1, 10);
+    [_captureDevice unlockForConfiguration];
+  } else {
+    NSLog(@"Could not lock device for config error: %@", configurationError);
+  }
+
   return self;
 }
 
